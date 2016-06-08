8225f3d35bd7feb85415669737730d8ad1463e07
Ramzi Kanazi <arcan3xx>
2016-06-08 10:36:54 -0400

    updating for eight ball

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('button').click(function(event) {
+    $.fn.extend({
+        animateCss: function (animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+            });
+        }
+    });
+    $('img').animateCss('rollIn');
+  });
+});
